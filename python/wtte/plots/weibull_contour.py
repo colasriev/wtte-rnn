12c17c0ff101f938ed5133f2a60739babc62f4a1
Colas Riviere <colas.riviere.external@eviden.com>
2024-05-30 17:28:41 +0200

    black

--- python/wtte/plots/weibull_contour.py
+++ python/wtte/plots/weibull_contour.py
@@ -9,7 +9,9 @@ from six.moves import xrange
 from wtte import weibull
 
 
-def weibull_contour(Y, U, is_discrete, true_alpha, true_beta, logx=True, samples=200, lines=True):
+def weibull_contour(
+    Y, U, is_discrete, true_alpha, true_beta, logx=True, samples=200, lines=True
+):
 
     xlist = np.linspace(true_alpha / np.e, true_alpha * np.e, samples)
     ylist = np.linspace(true_beta / np.e, true_beta * np.e, samples)
@@ -23,8 +25,7 @@ def weibull_contour(Y, U, is_discrete, true_alpha, true_beta, logx=True, samples
         fun = weibull.continuous_loglik
 
     for i in xrange(len(Y)):
-        loglik = loglik + \
-            fun(Y[i], x_grid, y_grid, U[i])
+        loglik = loglik + fun(Y[i], x_grid, y_grid, U[i])
 
     z_grid = loglik / len(Y)
 
@@ -32,14 +33,14 @@ def weibull_contour(Y, U, is_discrete, true_alpha, true_beta, logx=True, samples
     if logx:
         x_grid = np.log(x_grid)
         true_alpha = np.log(true_alpha)
-        xlab = r'$\log(\alpha)$'
+        xlab = r"$\log(\alpha)$"
     else:
-        xlab = r'$\alpha$'
+        xlab = r"$\alpha$"
 
-    cp = plt.contourf(x_grid, y_grid, z_grid, 100, cmap='jet')
+    cp = plt.contourf(x_grid, y_grid, z_grid, 100, cmap="jet")
     plt.colorbar(cp)
     if lines:
-        plt.axvline(true_alpha, linestyle='dashed', c='black')
-        plt.axhline(true_beta, linestyle='dashed', c='black')
+        plt.axvline(true_alpha, linestyle="dashed", c="black")
+        plt.axhline(true_beta, linestyle="dashed", c="black")
     plt.xlabel(xlab)
-    plt.ylabel(r'$\beta$')
+    plt.ylabel(r"$\beta$")
--- python/wtte/plots/weibull_heatmap.py
+++ python/wtte/plots/weibull_heatmap.py
@@ -9,32 +9,36 @@ from six.moves import xrange
 from wtte import weibull
 
 
-def basic_heatmap(ax, pred, max_horizon=None, resolution=None, cmap='jet'):
+def basic_heatmap(ax, pred, max_horizon=None, resolution=None, cmap="jet"):
     if max_horizon is None:
         max_horizon = pred.shape[0]
 
     if resolution is None:
         resolution = max_horizon
 
-    ax.imshow(pred.T, origin='lower', interpolation='none',
-              aspect='auto', cmap=cmap)
-    ax.set_yticks([x * (resolution + 0.0) /
-                   max_horizon for x in [0, max_horizon / 2, max_horizon - 1]])
+    ax.imshow(pred.T, origin="lower", interpolation="none", aspect="auto", cmap=cmap)
+    ax.set_yticks(
+        [
+            x * (resolution + 0.0) / max_horizon
+            for x in [0, max_horizon / 2, max_horizon - 1]
+        ]
+    )
     ax.set_yticklabels([0, max_horizon / 2, max_horizon - 1])
     ax.set_ylim(-0.5, resolution - 0.5)
-    ax.set_ylabel('steps ahead $s$')
+    ax.set_ylabel("steps ahead $s$")
     return ax
 
 
 def weibull_heatmap(
-    fig, ax,
+    fig,
+    ax,
     xx,
     alpha,
     beta,
     max_horizon,
     time_to_event=None,
     censoring_indicator=None,
-    title='predicted Weibull pmf $p(t,s)$',
+    title="predicted Weibull pmf $p(t,s)$",
     lw=None,
     is_discrete=True,
     resolution=None,
@@ -42,11 +46,11 @@ def weibull_heatmap(
     color="red",
 ):
     """
-        Adds a continuous or discrete heatmap with TTE to ax.
+    Adds a continuous or discrete heatmap with TTE to ax.
 
-        Caveats:
-        - axis are pixels so axis's always discrete.
-          (so we want location of labels to be in middle)
+    Caveats:
+    - axis are pixels so axis's always discrete.
+      (so we want location of labels to be in middle)
     """
     if resolution is None:
         # Resolution. Defaults to 1/step. Want more with pdf.
@@ -65,7 +69,7 @@ def weibull_heatmap(
             raise NotImplementedError("Implement me")
 
     # print(f"FFF {is_censored}")
-    
+
     # Number of timesteps
     xx_length = len(xx)
     assert len(alpha) == xx_length
@@ -76,40 +80,43 @@ def weibull_heatmap(
     weibull_values = prob_fun(
         np.tile(np.linspace(0, max_horizon - 1, resolution), (xx_length, 1)),
         np.tile(alpha.reshape(-1, 1), (1, resolution)),
-        np.tile(beta.reshape(-1, 1), (1, resolution))
+        np.tile(beta.reshape(-1, 1), (1, resolution)),
     )
 
-    ax = basic_heatmap(ax, weibull_values, max_horizon, resolution,
-                       cmap=cmap)
+    ax = basic_heatmap(ax, weibull_values, max_horizon, resolution, cmap=cmap)
     ax.set_title(title)
 
-    def ax_add_scaled_line(ax, t, y, y_value_max, y_n_pixels, label,
-                           color, linestyle=None):
+    def ax_add_scaled_line(
+        ax, t, y, y_value_max, y_n_pixels, label, color, linestyle=None
+    ):
         # Shifts and scales y to fit on an imshow as we expect it to be, i.e
         # passing through middle of a pixel
         scaled_y = ((y_n_pixels + 0.0) / y_value_max) * y
-        ax.plot(t - 0.5, scaled_y, lw=lw,
-                color=color, label=label, linestyle=linestyle)
+        ax.plot(t - 0.5, scaled_y, lw=lw, color=color, label=label, linestyle=linestyle)
 
     if time_to_event is not None:
         if not all(~is_censored):
-            ax_add_scaled_line(ax,
-                               xx,
-                               np.ma.array(time_to_event, mask=is_censored),
-                               y_value_max=max_horizon,
-                               y_n_pixels=resolution,
-                               color=color,
-                               label='TTE')
-            ax_add_scaled_line(ax,
-                               xx,
-                               np.ma.array(time_to_event, mask=~is_censored),
-                               y_value_max=max_horizon,
-                               y_n_pixels=resolution,
-                               color=color,
-                               linestyle=":",
-                               label='TTE (censored)')
+            ax_add_scaled_line(
+                ax,
+                xx,
+                np.ma.array(time_to_event, mask=is_censored),
+                y_value_max=max_horizon,
+                y_n_pixels=resolution,
+                color=color,
+                label="TTE",
+            )
+            ax_add_scaled_line(
+                ax,
+                xx,
+                np.ma.array(time_to_event, mask=~is_censored),
+                y_value_max=max_horizon,
+                y_n_pixels=resolution,
+                color=color,
+                linestyle=":",
+                label="TTE (censored)",
+            )
 
-    ax.set_xlabel('time')
+    ax.set_xlabel("time")
     fig.tight_layout()
 
     return fig, ax
